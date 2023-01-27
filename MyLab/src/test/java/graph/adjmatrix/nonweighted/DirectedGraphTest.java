a9df34b233c1e5964659e45ec5e4b5d790776f61
skydreamer21 <skydreamer210@gmail.com>
2023-01-27 21:46:58 +0900

    create: 그래프 구현 틀 생성

---
+++ MyLab/src/test/java/graph/adjmatrix/nonweighted/DirectedGraphTest.java
@@ -0,0 +1,12 @@
+package graph.adjmatrix.nonweighted;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class DirectedGraphTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
